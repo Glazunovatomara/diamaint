ad047a6169f3b5188c30ebbbc498df3c329f7623
ElenLen <relax-pl@yandex.ru>
2024-11-12 20:37:05 +0500

    feat: update Burger

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,10 @@
 const burger = document.querySelector(".header__menu-burger");
 const menu = document.querySelector(".header__menu-list");
 
+// const burger = document.querySelector(".hamburger1");
+const menu1 = document.querySelector(".menu");
+
 burger.addEventListener("click", () => {
+  menu1.classList.toggle("active");
   menu.classList.toggle("active");
 });
