fec6f90a126458648e68b47d991c706f9676b422
NasheSiyatelstvo <165139814+NasheSiyatelstvo@users.noreply.github.com>
2024-11-14 15:42:50 +0100

    unit_6_and_footer

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,3 +6,41 @@ const menu = document.querySelector(".header__menu-list");
 burger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let currentImageIndex = 0;
+
+  function showSlide(index) {
+      const images = document.querySelectorAll('.author-images img');
+      const totalImages = images.length;
+
+      if (index >= totalImages - 1) {
+          currentImageIndex = totalImages - 1;
+          document.querySelector('.next').disabled = true;
+      } else if (index <= 0) {
+          currentImageIndex = 0;
+          document.querySelector('.prev').disabled = true;
+      } else {
+          currentImageIndex = index;
+          document.querySelector('.next').disabled = false;
+          document.querySelector('.prev').disabled = false;
+      }
+
+      document.querySelector('.author-images').style.transform = `translateX(-${currentImageIndex * 100}%)`;
+  }
+
+  function nextSlide() {
+      showSlide(currentImageIndex + 1);
+  }
+
+  function prevSlide() {
+      showSlide(currentImageIndex - 1);
+  }
+
+  // Назначаем обработчики событий для кнопок
+  document.querySelector('.next').addEventListener('click', nextSlide);
+  document.querySelector('.prev').addEventListener('click', prevSlide);
+
+  // Показываем первое изображение при загрузке
+  showSlide(currentImageIndex);
+});
