dd03c5538ff65ab39b4b90f74b69be77f8ed5de0
ochumenno66 <ochumenno66@yandex.ru>
2024-11-16 01:21:50 +0300

    alena_adaptiv_5

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,56 +9,58 @@ burger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let currentImageIndex = 0;
 
   function showSlide(index) {
-      const images = document.querySelectorAll('.author-images img');
-      const totalImages = images.length;
+    const images = document.querySelectorAll(".author-images img");
+    const totalImages = images.length;
 
-      if (index >= totalImages - 1) {
-          currentImageIndex = totalImages - 1;
-          document.querySelector('.next').disabled = true;
-      } else if (index <= 0) {
-          currentImageIndex = 0;
-          document.querySelector('.prev').disabled = true;
-      } else {
-          currentImageIndex = index;
-          document.querySelector('.next').disabled = false;
-          document.querySelector('.prev').disabled = false;
-      }
+    if (index >= totalImages - 1) {
+      currentImageIndex = totalImages - 1;
+      document.querySelector(".next").disabled = true;
+    } else if (index <= 0) {
+      currentImageIndex = 0;
+      document.querySelector(".prev").disabled = true;
+    } else {
+      currentImageIndex = index;
+      document.querySelector(".next").disabled = false;
+      document.querySelector(".prev").disabled = false;
+    }
 
-      document.querySelector('.author-images').style.transform = `translateX(-${currentImageIndex * 100}%)`;
+    document.querySelector(".author-images").style.transform = `translateX(-${
+      currentImageIndex * 100
+    }%)`;
   }
 
   function nextSlide() {
-      showSlide(currentImageIndex + 1);
+    showSlide(currentImageIndex + 1);
   }
 
   function prevSlide() {
-      showSlide(currentImageIndex - 1);
+    showSlide(currentImageIndex - 1);
   }
 
   // Назначаем обработчики событий для кнопок
-  document.querySelector('.next').addEventListener('click', nextSlide);
-  document.querySelector('.prev').addEventListener('click', prevSlide);
+  document.querySelector(".next").addEventListener("click", nextSlide);
+  document.querySelector(".prev").addEventListener("click", prevSlide);
 
   // Показываем первое изображение при загрузке
   showSlide(currentImageIndex);
 });
 
-// swiper slider block 2 
-const swiper = new Swiper('.swiper', {
-    spaceBetween:20,
-    slidesPerView: 1,
+// swiper slider block 2
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 20,
+  slidesPerView: 1,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  //BLOCK4
+//BLOCK4
 const modal = document.getElementById("publicationMyModal");
 const hoverTextBlock = document.getElementById("hoverTextBlock");
 const modalBodyContent = modal.querySelector(".publication__modal_body");
